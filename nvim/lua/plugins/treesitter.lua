9889e7e99b2670f08abc63f1d51146001a789fe7
aaronkoichi <aaronlee4399@gmail.com>
2024-01-01 18:23:18 +0800

    added none-ls + alpha-nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ config = function()
 	local configs = require("nvim-treesitter.configs")
 	configs.setup({
 		ensure_installed = {"vim", "vimdoc", "cmake", "lua", "javascript", "java", "c", "c_sharp", "cpp", "html", "css"},
+		auto_install = true, -- installs any new language everytime TS has encountered. 	
 		highlight = {enable = true},
 		indent = {enable = true},
 	})
