75ab5f824293369a97dcc28cd43df8c9a9b065e4
aaronkoichi <aaronlee4399@gmail.com>
2024-01-04 12:55:49 +0800

    Added undotree/harpoon/git-view via vim-fugitive. Turn off auto-save to use harpoon.

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -4,7 +4,7 @@ return {
 		 require("auto-save").setup {
 			-- your config goes here
 			-- or just leave it empty :)
-			vim.api.nvim_set_keymap("n", "<leader>a", ":ASToggle<CR>", {})
+			vim.api.nvim_set_keymap("n", "<leader>as", ":ASToggle<CR>", {})
 		 }
 	end,
 }
